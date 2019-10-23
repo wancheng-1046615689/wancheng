971d71cb72b83408d300d9e74a47948bffb1e2aa
wancheng-1046615689 <56863973+wancheng-1046615689@users.noreply.github.com>
2019-10-23 10:42:11 +0800

    Update test.java

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -5,8 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		String aa = "aa";
 		String bb = "B";
-		String cc = "c";
-		String dd = "c";
+		
 		System.out.println();
 		String ff = "c";
 		
