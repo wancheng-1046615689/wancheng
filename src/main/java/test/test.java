7d903cb19fd2cf481c014ecd97efd925e797c633
cheng.wan <1046615689@qq.com>
2019-10-23 10:41:20 +0800

    aa

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -4,6 +4,11 @@ public class test {
 
 	public static void main(String[] args) {
 		String aa = "aa";
-		String bb = "aa";
+		String bb = "B";
+		String cc = "c";
+		String dd = "c";
+		System.out.println();
+		String ff = "c";
+		
 	}
 }
