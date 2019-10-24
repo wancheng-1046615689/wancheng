9d6e9f7adb00c84a8cda0a53d6b1abd51fdd226b
cheng.wan <1046615689@qq.com>
2019-10-24 17:44:30 +0800

    回滚

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -4,17 +4,6 @@ public class test {
 
 	public static void main(String[] args) {
 		String aa = "aa";
-		String bb = "B";
-		String cc = "c";
-		String dd = "c";
-		String ff = "c";
-		String ac = "c";
-		String fc = "c";
-		System.out.println();
-		String dc = "b";
-		String gg = "g";
-		String hh = "h";
 
-			
 	}
 }
